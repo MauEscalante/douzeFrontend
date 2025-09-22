a3b4a992d81a69676b6ec75d0152f87eb00e35c9
MauEscalante <Escalantemauricio2002@gmail.com>
2025-09-21 21:01:28 -0300

    Diseño Login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Components/HomePage";
+import Login from "./Components/Login.js";
 import Navbar from "./Components/Navbar.js";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
             { <Navbar />}
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </>
     );
